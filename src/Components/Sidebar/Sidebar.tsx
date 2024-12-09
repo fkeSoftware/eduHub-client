ae568693cce3b50281f6c04223fbf36dd87353fe
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-12-09 16:41:58 +0300

    modified sidebar & created sidebar links

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,14 +1,68 @@
-import React from "react";
-import "./sidebar.css"
-type Props = {};
+import React, { useState } from "react";
+import "./sidebar.css";
+import { Link } from "react-router-dom";
+import { menuItems } from "./SidebarMenu";
+import { RiCheckboxBlankCircleFill, RiShutDownLine  } from "react-icons/ri";
+import cat from "../../assets/user__cat.png"
+import logoFeyza from "../../assets/profile_p.jpg"
+const Sidebar = () => {
+  const [expandedMenu, setExpandedMenu] = useState<string | null>(null);
 
-const Sidebar = (props: Props) => {
+  const toggleExpand = (id: string) => {
+    setExpandedMenu((prev) => (prev === id ? null : id));
+  };
+
   return (
-    <>
-      <div className="component-layout sidebar-layout">
-      <div className="sub-head">Sidebar</div>
+    <div className="component-layout sidebar-layout d-grid justify-space-between align-i-f-start padding-1">
+      <div className="sidebar-layout__user d-flex f-direction-row justify-space-evenly align-i-center o-hidden gap-2">
+        <Link to="" className=" profile-sidebar f-direction-row d-flex align-i-center justify-center o-hidden">
+          <img src={logoFeyza} alt="" className="w48"/></Link>
+        <div className="sidebar-layout__user__info__container d-flex f-direction-column">
+          <Link className="text text-dark-mode" to="">Feyza KARANFİL ERAT</Link>
+          <small className="small-text small-text-dark-mode italic">IT Manager</small>
+        </div>
       </div>
-    </>
+      <div className="sidebar-layout__link__container d-flex f-direction-column">
+        {menuItems.map((menu) => (
+          <div
+            key={menu.id}
+            className="sidebar-layout__link f-direction-column"
+          >
+            <div className="d-flex f-direction-row align-i-center gap-1">
+              <span className="icon">{menu.icon}</span>
+              <Link
+                to="#"
+                className="big-text-dark-mode d-flex align-i-center"
+                onClick={() => toggleExpand(menu.id)}
+              >
+                {""}
+                <span className="title">{menu.title}</span>
+              </Link>
+            </div>
+            <div
+              className={`sidebar-layout__link__submenu d-flex f-direction-column  ${
+                expandedMenu === menu.id ? "is-visible" : ""
+              }`}
+            >
+              {menu.links.map((link) => (
+                <Link className="text-dark-mode" key={link.path} to={link.path}>
+                  <span>
+                    <RiCheckboxBlankCircleFill />
+                  </span>
+                  <span>
+                    {" "}
+                    <b>{link.label}</b>
+                  </span>
+                </Link>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="sidebar-layout__action text-right">
+        <Link to="" className="first-head text-warning"><RiShutDownLine /></Link>
+      </div>
+    </div>
   );
 };
 
---
+++ src/Components/Sidebar/SidebarMenu.tsx
@@ -0,0 +1,98 @@
+import { ReactNode } from "react";
+import { PiStudentFill,PiChalkboardTeacher } from "react-icons/pi";
+import { BiSolidCategory  } from "react-icons/bi";
+import { FiUsers } from "react-icons/fi";
+import { BsCalendarEvent } from "react-icons/bs";
+import { MdOutlineSettings } from "react-icons/md";
+
+export type MenuItem = {
+  id: string;
+  icon: ReactNode;
+  title: string;
+  links: { path: string; label: string }[];
+};
+
+export const menuItems: MenuItem[] = [
+  {
+    id: "settings",
+    icon: <MdOutlineSettings  />,
+    title: "Settings",
+    links: [
+      { path: "/mail-configuration", label: "Mail Configuration" },
+      { path: "/theme-configuration", label: "Theme Configuration" },
+      { path: "/site-settings", label: "Site Settings" },
+    ],
+  },
+  {
+    id: "users",
+    icon: <FiUsers />,
+    title: "Users",
+    links: [
+      { path: "/user-list", label: "User List" },
+      { path: "/permissions", label: "Permissions" },
+    ],
+  },
+  {
+    id: "students",
+    icon: <PiStudentFill />,
+    title: "Students",
+    links: [
+      { path: "/student-list", label: "Student List" },
+      { path: "/exams", label: "Exams" },
+      { path: "/courses", label: "Courses" },
+    ],
+  },
+  {
+    id: "teachers",
+    icon: <PiChalkboardTeacher />,
+    title: "Teachers",
+    links: [
+      { path: "/teacher-list", label: "Teacher List" },
+      { path: "/classes", label: "Classes" },
+      { path: "/documents", label: "Documents" },
+    ],
+  },
+  {
+    id: "courses",
+    icon: <BiSolidCategory  />,
+    title: "Courses",
+    links: [
+      { path: "/math", label: "Mathematics" },
+      { path: "/grammar", label: "Grammar" },
+      { path: "/science-knowledge", label: "Science Knowledge" },
+      { path: "/social-sciences", label: "Social Sciences" },
+      { path: "/information-technologies", label: "Information-Technologies" },
+      { path: "/music", label: "Music" },
+      { path: "/art", label: "Art" },
+      { path: "/guidance", label: "Guidance" },
+    ],
+  },
+  {
+    id: "classrooms",
+    icon: <PiChalkboardTeacher />,
+    title: "Classrooms",
+    links: [
+      { path: "/classrooms", label: "Classrooms" },
+      { path: "/laboratories", label: "Laboratories" },
+      { path: "/study-rooms", label: "Study Rooms" },
+      { path: "/conference-halls", label: "conference Halls" },
+      { path: "/libraries", label: "Libraries" },
+      { path: "/art-rooms", label: "Art Rooms" },
+      { path: "/gyms", label: "Gyms" },
+      { path: "/teachers-rooms", label: "Teachers' rooms" },
+      { path: "/staff-rooms", label: "Staff Rooms" },
+      { path: "/store-rooms", label: "Store Rooms" },
+      { path: "/server-room", label: "Server Room" },
+    ],
+  },
+  {
+    id: "events",
+    icon: <BsCalendarEvent  />,
+    title: "Events",
+    links: [
+      { path: "/event-list", label: "Event List" },
+      { path: "/event-announcements", label: "Event Announcements" },
+      { path: "/galleries", label: "Galleries" },
+    ],
+  },
+];

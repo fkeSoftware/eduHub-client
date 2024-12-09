91c1cd1a7ccc49c81d54a9e5fef2a8ec8f6bdee9
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-12-09 10:59:38 +0300

    modified footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import "./footer.css";
+import { Link } from "react-router-dom";
+import logo from "../../assets/fke-logo-light.png";
 type Props = {};
 
 const Footer = (props: Props) => {
-  return (
-    <>
-      <div className="component-layout footer-layout fixed text-center">
-        <div className="sub-head">Footer</div>
+    const currentYear = new Date().getFullYear();
+    return (
+      <div className="footer-layout text-center p-05 d-flex justify-center align-i-center relative padding-025 o-hidden">
+        <div className=" d-flex justify-center align-i-center">
+          <Link
+            to="https://feyzaerat.com.tr"
+            className="small-text text-dark-mode d-flex justify-center align-i-center"
+          >
+            <span>
+              {" "}
+              <img className="w48 margin-025" src={logo} alt="fke-logo" />
+            </span>
+            ©{currentYear} All rights reserved
+          </Link>
+        </div>
       </div>
-    </>
   );
 };
 

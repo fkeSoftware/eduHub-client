f003ed074045b711ea10618ac95106bd6be102bb
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2024-12-11 21:24:16 +0300

    modified input css & created some base styles

--- src/Components/Formik/FormikInput.tsx
+++ src/Components/Formik/FormikInput.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { Field, Formik } from "formik";
+import React from "react";
 
-type Props = {}
+type Props = {
+    label?: string;
+    name: string;
+    type?:string;
+    disabled?:any;
+    style?:any
+    placeHolder:string
+};
 
 const FormikInput = (props: Props) => {
+  
   return (
-    <div>FormikInput</div>
-  )
-}
+    <>
+     <div className="input__container  d-flex f-direction-column align-i-flex-start justify-center gap-1">
+        <label className="input__container__label text text-dark-mode">{props.label}</label>
+        <Field 
+        name={props.name}
+        type={props.type || "text"}
+        className="input__container__input text text-dark-mode"
+        disabled={props.disabled}
+        placeHolder={props.placeHolder}
+        style={props.style}
+        />
+     </div>
+      
+    </>
+  );
+};
 
-export default FormikInput
+export default FormikInput;
